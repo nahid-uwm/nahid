44063e8dec7b8887db02eafe0b55fe2fd8ffbb8e
nahid-uwm <hasan26@uwm.edu>
2026-03-16 15:24:59 -0500

    redirects

---
+++ js/redirects.js
@@ -0,0 +1,9 @@
+// Add your dynamic redirects in this file.
+// Format: 
+// "/path-name": "https://destination-link.com"
+// Make sure to use lowercase for the path names (e.g., "/googlescholar")
+
+const REDIRECTS = {
+    "/googlescholar": "https://scholar.google.com/citations?user=xQR0_1QAAAAJ&hl",
+    "/projectname": "https://example.com/your-project-link"
+};
